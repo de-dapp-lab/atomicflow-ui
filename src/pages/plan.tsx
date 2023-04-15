0a30582be769d936288d71e71911143129fb75b3
ten3 <ken.osada@dena.com>
2023-04-15 17:36:38 +0900

    require wallet connect access to dashboad and plan page

--- src/pages/plan.tsx
+++ src/pages/plan.tsx
@@ -7,24 +7,42 @@ import WeeklyRevenue from '@/components/weeklyRevenue/weeklyRevenue';
 import Banner from '@/components/banner/banner';
 import SelectPlanBanner from '../components/Plan/selectPlanBanner';
 import SettingPlan from '../components/Plan/settingPlan';
+import {useAccount} from "wagmi";
+import LoginPage from "@/components/view/loginPage";
 
 const Plan: NextPage = () => {
 
+	const { address } = useAccount();
 	const [selectPlan,useSelectPlan] = useState("")
 	const [planName,usePlanName] = useState("")
 	const [maxMember,useMaxMember] = useState(1)
 	const [amountPerMonth,useAmountPerMonth] = useState(1)
 
     return (
-        <PlanLayout >
-            <br></br>
-            <br></br>
-            <br></br>
-			<Grid h='100%' templateColumns='repeat(5, 1fr)' templateRows='repeat(1, 1fr)' gap='20px' mb='20px'>
-				<GridItem colSpan={2}>
-					<SelectPlanBanner 
+		<>
+		{ address === undefined && <LoginPage/> }
+		{ address !== undefined && (
+			<PlanLayout >
+				<br></br>
+				<br></br>
+				<br></br>
+				<Grid h='100%' templateColumns='repeat(5, 1fr)' templateRows='repeat(1, 1fr)' gap='20px' mb='20px'>
+					<GridItem colSpan={2}>
+						<SelectPlanBanner
+							selectPlan={selectPlan}
+							useSelectPlan={useSelectPlan}
+							planName={planName}
+							usePlanName={usePlanName}
+							maxMember={maxMember}
+							useMaxMember={useMaxMember}
+							amountPerMonth={amountPerMonth}
+							useAmountPerMonth={useAmountPerMonth}
+						/>
+					</GridItem>
+					<GridItem colSpan={3}>
+					<SettingPlan
 						selectPlan={selectPlan}
-						useSelectPlan={useSelectPlan} 
+						useSelectPlan={useSelectPlan}
 						planName={planName}
 						usePlanName={usePlanName}
 						maxMember={maxMember}
@@ -32,21 +50,11 @@ const Plan: NextPage = () => {
 						amountPerMonth={amountPerMonth}
 						useAmountPerMonth={useAmountPerMonth}
 					/>
-				</GridItem>
-				<GridItem colSpan={3}>
-				<SettingPlan 
-					selectPlan={selectPlan}
-					useSelectPlan={useSelectPlan} 
-					planName={planName}
-					usePlanName={usePlanName}
-					maxMember={maxMember}
-					useMaxMember={useMaxMember}
-					amountPerMonth={amountPerMonth}
-					useAmountPerMonth={useAmountPerMonth}
-				/>
-				</GridItem>
-			</Grid>
-        </PlanLayout>
+					</GridItem>
+				</Grid>
+			</PlanLayout>
+		)}
+		</>
     )
 }
 
